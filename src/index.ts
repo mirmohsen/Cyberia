1d322661d0e5a9bb02b891884ccb2b1d8ec61b7d
mohsen <mirmohsenmirazimitarki78@gmail.com>
2025-05-27 17:02:38 +0330

    added express.json

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
+import router from './router/v1/router';
 
 import './model/setup/mongodb';
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-	res.end('welcome to task cyberia');
-});
+app.use(express.json());
+app.use('/api', router);
 
 app.listen(port, () => {
 	console.log(`---> server up on port:${port}`);
